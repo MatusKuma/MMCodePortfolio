2b343337e72cfb3219ac02df5c2e7f39ba3ce125
MatusKuma <xkuma@stuba.sk>
2025-08-28 18:33:31 +0200

    add navbar

--- codejungle/src/components/Navbar.js
+++ codejungle/src/components/Navbar.js
@@ -1,11 +1,14 @@
-
+import '../Styles/Navbar.css'
 
 function Navbar() {
     return (
         <div>
+            <ul>
             <li>Home</li>
             <li>Our Projects</li>
             <li>Contact</li>
+            </ul>
+            
         </div>
         );
 }
