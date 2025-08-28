bee81f5cc4e56e2c75439beb8b35d0c9755740db
MatusKuma <xkuma@stuba.sk>
2025-08-28 18:43:56 +0200

    rename footer

--- codejungle/src/App.js
+++ codejungle/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
-import Footer from './components/footer.js';
+import Footer from './components/Footer.js';
 function App() {
   return (
     <div className="App">
