de9e3f8cc1268c7339ac421649f107a9421b7ea7
MatusKuma <xkuma@stuba.sk>
2025-08-28 18:17:50 +0200

    create navbar component

---
+++ codejungle/src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+
+function Navbar() {
+    return (
+        <div>
+            <li>Home</li>
+            <li>Our Projects</li>
+            <li>Contact</li>
+        </div>
+        );
+}
+
+
+export default Navbar;
