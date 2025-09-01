a132aac1444f4546daa21b81388f1bec16d33ced
MatusKuma <xkuma@stuba.sk>
2025-09-01 13:54:19 +0200

    fix BrowserRouter

--- mmcode/src/App.js
+++ mmcode/src/App.js
@@ -3,27 +3,17 @@ import './App.css';
 import Projects from './pages/Projects.js';
 import Contacts from './pages/Contacts.js';
 import HomePage from './pages/HomePage.js';
+
 function App() {
   return (
-    <>
-     <BrowserRouter>
-       <Routes>
-        <Route index element={<HomePage/>}/>
+    <BrowserRouter basename="/MMCodePortfolio">
+      <Routes>
+        <Route index element={<HomePage />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Contacts" element={<Contacts />} />
-        <Route path="/*" element={<Projects />} />
-        
+        <Route path="/*" element={<HomePage />} /> {/* fallback */}
       </Routes>
     </BrowserRouter>
-      
-    </>
-    //vytvorit HP
-    //krajsi navbar
-    //poriesit UI
-    //update routeru
-
-
-  
   );
 }
 
