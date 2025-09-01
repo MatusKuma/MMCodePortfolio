0d1e2ee8736a579c2776765a9feb352e2098f2bf
MatusKuma <xkuma@stuba.sk>
2025-09-01 13:53:48 +0200

    add logo to navbar

--- mmcode/src/components/Navbar.js
+++ mmcode/src/components/Navbar.js
@@ -1,14 +1,23 @@
-import '../styles/Navbar.css'
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+import logo from '../assets/logo_mmcode.png';
+
 function Navbar() {
-    return (
-        <div className='Nav'>
-            <ul>
-                <li><a href="/">Domov</a></li>
-                <li><a href="/Projects">Projekty</a></li>
-                <li><a href="/Contacts">Kontakty</a></li>
-            </ul>
-            
-        </div>
-        );
+  return (
+    <div className='Nav'>
+      <div className="Nav-container">
+        <Link to="/" className="Nav-logo">
+          <img src={logo} alt="Logo MMCode" />
+          <span className="Nav-logo-text">MMCode</span>
+        </Link>
+        <ul className="Nav-links">
+          <li><Link to="/">Domov</Link></li>
+          <li><Link to="/Projects">Naše projekty</Link></li>
+          <li><Link to="/Contacts">Kontakt</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
 export default Navbar;
